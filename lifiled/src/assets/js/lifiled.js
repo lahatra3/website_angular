ad878e847f91f78ab913a0f4f59deca950c7f05c
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-10-12 16:21:55 +0300

    Ajout de preloader...

--- lifiled/src/assets/js/lifiled.js
+++ lifiled/src/assets/js/lifiled.js
@@ -1,7 +1,13 @@
 var cursor = document.getElementById('cursor');
 const header = document.querySelector('#navigation');
+const preloader = document.querySelector('#preloader');
+
 document.onmousemove = (e) => {
     cursor.style.left = (e.pageX - 1) + 'px';
     cursor.style.top = (e.pageY - 1) + 'px';
     cursor.style.display = 'block';
 }
+
+window.addEventListener('load', () => {
+    preloader.style.display = 'none';
+});
